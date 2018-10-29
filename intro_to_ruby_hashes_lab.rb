421d481cf908556201c27a4c2537f17ef958b6d3
Joaquim Barreto <joaquim@barreto.co>
2018-10-29 01:00:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,9 @@ end
 def monopoly
 	monopoly = {
 	  :railroads => {
-	    :pieces=>4,
-	    :names=>{}
-      :rent_in_dollars=>{}
+	    :pieces => 4,
+	    :names => {}
+      :rent_in_dollars => {}
 	  }
 	}
 
